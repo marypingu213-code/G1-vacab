6c343e0546112001ac2ac8a1f4b477c049518ca5
Carz <marypingu213@gmail.com>
2026-03-13 19:16:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -25,7 +25,7 @@ const allLessonsData = {
             },
             { 
                 word: "布", parts: ["images/L3/筆順-布-部件-1.png", "images/L3/筆順-布-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}],
-                vocab: "布幕", vocabImg: "images/L3/vocab-布丁.png"
+                vocab: "布丁", vocabImg: "images/L3/vocab-布丁.png"
             },
             { 
                 word: "場", parts: ["images/L3/筆順-場-部件-1.png", "images/L3/筆順-場-部件-2.png", "images/L3/筆順-場-部件-3.png", "images/L3/筆順-場-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
@@ -45,15 +45,15 @@ const allLessonsData = {
             },
             { 
                 word: "風", parts: ["images/L3/筆順-風-部件-1.png", "images/L3/筆順-風-部件-2.png", "images/L3/筆順-風-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
-                vocab: "微風", vocabImg: "images/L3/vocab-龍捲風.png"
+                vocab: "龍捲風", vocabImg: "images/L3/vocab-龍捲風.png"
             },
             { 
                 word: "跑", parts: ["images/L3/筆順-跑-部件-1.png", "images/L3/筆順-跑-部件-2.png", "images/L3/筆順-跑-部件-3.png", "images/L3/筆順-跑-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
-                vocab: "奔跑", vocabImg: "images/L3/vocab-跑步.png"
+                vocab: "跑步", vocabImg: "images/L3/vocab-跑步.png"
             },
             { 
                 word: "置", parts: ["images/L3/筆順-置-部件-1.png", "images/L3/筆順-置-部件-2.png", "images/L3/筆順-置-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
-                vocab: "布置", vocabImg: "images/L3/vocab-置物櫃.png"
+                vocab: "置物櫃", vocabImg: "images/L3/vocab-置物櫃.png"
             }
         ], 
         level2: [], level3: [], level4: [] 
