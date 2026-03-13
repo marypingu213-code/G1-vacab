a694048e126c041e983fe416e192e6cb0b8c0c51
Carz <marypingu213@gmail.com>
2026-03-13 18:24:08 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,35 +3,59 @@ const allLessonsData = {
     "3": { 
         title: "第三課：山中音樂會", 
         level1: [
-            // 2個部件：上下排
-            { word: "音", parts: ["images/L3/筆順-音-部件-1.png", "images/L3/筆順-音-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            // 4個部件：田字格排列 (補滿4個座標)
-            { word: "樂", parts: ["images/L3/筆順-樂-部件-1.png", "images/L3/筆順-樂-部件-2.png", "images/L3/筆順-樂-部件-3.png", "images/L3/筆順-樂-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
-            // 4個部件：田字格排列 (補滿4個座標)
-            { word: "會", parts: ["images/L3/筆順-會-部件-1.png", "images/L3/筆順-會-部件-2.png", "images/L3/筆順-會-部件-3.png", "images/L3/筆順-會-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
-            // 2個部件：左右排
-            { word: "坡", parts: ["images/L3/筆順-坡-部件-1.png", "images/L3/筆順-坡-部件-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
-            // 3個部件：橫排 (補滿3個座標)
-            { word: "忙", parts: ["images/L3/筆順-忙-部件-1.png", "images/L3/筆順-忙-部件-2.png", "images/L3/筆順-忙-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
-            // 2個部件：上下排
-            { word: "布", parts: ["images/L3/筆順-布-部件-1.png", "images/L3/筆順-布-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            // 4個部件：田字格排列
-            { word: "場", parts: ["images/L3/筆順-場-部件-1.png", "images/L3/筆順-場-部件-2.png", "images/L3/筆順-場-部件-3.png", "images/L3/筆順-場-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
-            // 3個部件：橫排
-            { word: "河", parts: ["images/L3/筆順-河-部件-1.png", "images/L3/筆順-河-部件-2.png", "images/L3/筆順-河-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
-            // 2個部件：左右排
-            { word: "林", parts: ["images/L3/筆順-林-部件-1.png", "images/L3/筆順-林-部件-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
-            // 2個部件：上下排
-            { word: "間", parts: ["images/L3/筆順-間-部件-1.png", "images/L3/筆順-間-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            // 3個部件：橫排
-            { word: "風", parts: ["images/L3/筆順-風-部件-1.png", "images/L3/筆順-風-部件-2.png", "images/L3/筆順-風-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
-            // 4個部件：田字格排列
-            { word: "跑", parts: ["images/L3/筆順-跑-部件-1.png", "images/L3/筆順-跑-部件-2.png", "images/L3/筆順-跑-部件-3.png", "images/L3/筆順-跑-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
-            // 3個部件：橫排
-            { word: "置", parts: ["images/L3/筆順-置-部件-1.png", "images/L3/筆順-置-部件-2.png", "images/L3/筆順-置-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] }
+            { 
+                word: "音", parts: ["images/L3/筆順-音-部件-1.png", "images/L3/筆順-音-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}],
+                vocab: "聲音", vocabImg: "images/L3/vocab-聲音.png" // 🌟 新增語詞與圖
+            },
+            { 
+                word: "樂", parts: ["images/L3/筆順-樂-部件-1.png", "images/L3/筆順-樂-部件-2.png", "images/L3/筆順-樂-部件-3.png", "images/L3/筆順-樂-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
+                vocab: "音樂", vocabImg: "images/L3/vocab-音樂.png"
+            },
+            { 
+                word: "會", parts: ["images/L3/筆順-會-部件-1.png", "images/L3/筆順-會-部件-2.png", "images/L3/筆順-會-部件-3.png", "images/L3/筆順-會-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
+                vocab: "音樂會", vocabImg: "images/L3/vocab-音樂會.png"
+            },
+            { 
+                word: "坡", parts: ["images/L3/筆順-坡-部件-1.png", "images/L3/筆順-坡-部件-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}],
+                vocab: "山坡", vocabImg: "images/L3/vocab-山坡.png"
+            },
+            { 
+                word: "忙", parts: ["images/L3/筆順-忙-部件-1.png", "images/L3/筆順-忙-部件-2.png", "images/L3/筆順-忙-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
+                vocab: "幫忙", vocabImg: "images/L3/vocab-幫忙.png"
+            },
+            { 
+                word: "布", parts: ["images/L3/筆順-布-部件-1.png", "images/L3/筆順-布-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}],
+                vocab: "布幕", vocabImg: "images/L3/vocab-布幕.png"
+            },
+            { 
+                word: "場", parts: ["images/L3/筆順-場-部件-1.png", "images/L3/筆順-場-部件-2.png", "images/L3/筆順-場-部件-3.png", "images/L3/筆順-場-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
+                vocab: "廣場", vocabImg: "images/L3/vocab-廣場.png"
+            },
+            { 
+                word: "河", parts: ["images/L3/筆順-河-部件-1.png", "images/L3/筆順-河-部件-2.png", "images/L3/筆順-河-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
+                vocab: "小河", vocabImg: "images/L3/vocab-小河.png"
+            },
+            { 
+                word: "林", parts: ["images/L3/筆順-林-部件-1.png", "images/L3/筆順-林-部件-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}],
+                vocab: "樹林", vocabImg: "images/L3/vocab-樹林.png"
+            },
+            { 
+                word: "間", parts: ["images/L3/筆順-間-部件-1.png", "images/L3/筆順-間-部件-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}],
+                vocab: "中間", vocabImg: "images/L3/vocab-中間.png"
+            },
+            { 
+                word: "風", parts: ["images/L3/筆順-風-部件-1.png", "images/L3/筆順-風-部件-2.png", "images/L3/筆順-風-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
+                vocab: "微風", vocabImg: "images/L3/vocab-微風.png"
+            },
+            { 
+                word: "跑", parts: ["images/L3/筆順-跑-部件-1.png", "images/L3/筆順-跑-部件-2.png", "images/L3/筆順-跑-部件-3.png", "images/L3/筆順-跑-部件-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}],
+                vocab: "奔跑", vocabImg: "images/L3/vocab-奔跑.png"
+            },
+            { 
+                word: "置", parts: ["images/L3/筆順-置-部件-1.png", "images/L3/筆順-置-部件-2.png", "images/L3/筆順-置-部件-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}],
+                vocab: "布置", vocabImg: "images/L3/vocab-布置.png"
+            }
         ], 
-        level2: [], 
-        level3: [], 
-        level4: [] 
+        level2: [], level3: [], level4: [] 
     }
 };
