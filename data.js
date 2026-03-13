79b88399da1f428c7c1b91dfcc1eb7aad2963ce4
Carz <marypingu213@gmail.com>
2026-03-13 18:09:54 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,20 +3,32 @@ const allLessonsData = {
     "3": { 
         title: "第三課：山中音樂會", 
         level1: [
-            // 📝 老師請在這裡替換您真實的圖片路徑和座標數量喔！這裡先幫您預設兩個部件作為版型
+            // 2個部件：上下排
             { word: "音", parts: ["images/L3/音-1.png", "images/L3/音-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            { word: "樂", parts: ["images/L3/樂-1.png", "images/L3/樂-2.png", "images/L3/樂-3.png", "images/L3/樂-4.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            { word: "會", parts: ["images/L3/會-1.png", "images/L3/會-2.png", "images/L3/會-3.png", "images/L3/會-4.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
+            // 4個部件：田字格排列 (補滿4個座標)
+            { word: "樂", parts: ["images/L3/樂-1.png", "images/L3/樂-2.png", "images/L3/樂-3.png", "images/L3/樂-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
+            // 4個部件：田字格排列 (補滿4個座標)
+            { word: "會", parts: ["images/L3/會-1.png", "images/L3/會-2.png", "images/L3/會-3.png", "images/L3/會-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
+            // 2個部件：左右排
             { word: "坡", parts: ["images/L3/坡-1.png", "images/L3/坡-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
-            { word: "忙", parts: ["images/L3/忙-1.png", "images/L3/忙-2.png", "images/L3/忙-3.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
+            // 3個部件：橫排 (補滿3個座標)
+            { word: "忙", parts: ["images/L3/忙-1.png", "images/L3/忙-2.png", "images/L3/忙-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
+            // 2個部件：上下排
             { word: "布", parts: ["images/L3/布-1.png", "images/L3/布-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            { word: "場", parts: ["images/L3/場-1.png", "images/L3/場-2.png" "images/L3/場-3.png" "images/L3/場-4.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
-            { word: "河", parts: ["images/L3/河-1.png", "images/L3/河-2.png", "images/L3/河-3.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
+            // 4個部件：田字格排列 (補上漏掉的逗號，補滿4個座標)
+            { word: "場", parts: ["images/L3/場-1.png", "images/L3/場-2.png", "images/L3/場-3.png", "images/L3/場-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
+            // 3個部件：橫排 (補滿3個座標)
+            { word: "河", parts: ["images/L3/河-1.png", "images/L3/河-2.png", "images/L3/河-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
+            // 2個部件：左右排
             { word: "林", parts: ["images/L3/林-1.png", "images/L3/林-2.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
+            // 2個部件：上下排
             { word: "間", parts: ["images/L3/間-1.png", "images/L3/間-2.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            { word: "風", parts: ["images/L3/風-1.png", "images/L3/風-2.png", "images/L3/風-3.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] },
-            { word: "跑", parts: ["images/L3/跑-1.png", "images/L3/跑-2.png", "images/L3/跑-3.png", "images/L3/跑-4.png"], pos: [{x: 80, y: 180}, {x: 150, y: 180}] },
-            { word: "置", parts: ["images/L3/置-1.png", "images/L3/置-2.png", "images/L3/置-3.png"], pos: [{x: 100, y: 110}, {x: 100, y: 180}] }
+            // 3個部件：橫排 (補滿3個座標)
+            { word: "風", parts: ["images/L3/風-1.png", "images/L3/風-2.png", "images/L3/風-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] },
+            // 4個部件：田字格排列 (補滿4個座標)
+            { word: "跑", parts: ["images/L3/跑-1.png", "images/L3/跑-2.png", "images/L3/跑-3.png", "images/L3/跑-4.png"], pos: [{x: 90, y: 130}, {x: 150, y: 130}, {x: 90, y: 180}, {x: 150, y: 180}] },
+            // 3個部件：橫排 (補滿3個座標)
+            { word: "置", parts: ["images/L3/置-1.png", "images/L3/置-2.png", "images/L3/置-3.png"], pos: [{x: 70, y: 180}, {x: 130, y: 180}, {x: 190, y: 180}] }
         ], 
         level2: [], 
         level3: [], 
